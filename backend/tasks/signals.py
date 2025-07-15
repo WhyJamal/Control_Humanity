9ce214e5604d2af300c87ef4bfb99eb8ad192c1d
Thanos-in <sayyodbee2006@gmail.com>
2025-07-15 21:17:36 +0500

    signals.py

--- backend/tasks/signals.py
+++ backend/tasks/signals.py
@@ -5,20 +5,20 @@ from django.db.models.signals import post_save
 from .models import Task
 from tasks.utils import send_telegram_message
 
-# @receiver(post_migrate)
-# def ensure_default_statuses(sender, **kwargs):
-#     if sender.name == "tasks":
-#         Status = apps.get_model("tasks", "Status")
-#         defaults = [
-#             ("Start",   0, True),
-#             ("Overdue", 1, False),
-#             ("Finish",  2, False),
-#         ]
-#         for name, order, is_def in defaults:
-#             Status.objects.get_or_create(
-#                 name=name,
-#                 defaults={'order': order, 'is_default': is_def}
-#             )
+@receiver(post_migrate)
+def ensure_default_statuses(sender, **kwargs):
+    if sender.name == "tasks":
+        Status = apps.get_model("tasks", "Status")
+        defaults = [
+            ("Start",   0, True),
+            ("Overdue", 1, False),
+            ("Finish",  2, False),
+        ]
+        for name, order, is_def in defaults:
+            Status.objects.get_or_create(
+                name=name,
+                defaults={'order': order, 'is_default': is_def}
+            )
 
 
 # @receiver(post_save, sender=Task)
