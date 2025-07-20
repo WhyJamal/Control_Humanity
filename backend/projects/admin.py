e8d80be080af2afe77d75f5bbd628355c157f8b3
Thanos-in <sayyodbee2006@gmail.com>
2025-07-20 16:57:35 +0500

    fix(admin): correct tuple format in ModuleInline fields and enhance Project inlines
    - Fixed 'fields' in ModuleInline to be a proper tuple
    - Ensured both TaskInline and ModuleInline are included in ProjectAdmin"

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
 from .models import Project, Module
-from django.utils.translation import gettext_lazy as _
 from import_export.admin import ImportExportModelAdmin
+from tasks.models import Task
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    fields = ('title', 'assigned_to', 'status', 'is_archived')
+    extra = 0
+    show_change_link = True
+
+class ModuleInline(admin.TabularInline):
+    model = Module
+    fields = ('name',)
+    extra = 0
+    show_change_link = True
 
 @admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(ImportExportModelAdmin):
     list_display = ('name', 'organization', 'director', 'manager', 'start_date', 'end_date')  
     list_filter = ('start_date', 'organization', 'director', 'manager')  
     search_fields = ('name', 'description', 'director__username', 'manager__username')
     autocomplete_fields = ('director', 'manager')  
-
     date_hierarchy = 'start_date'
     list_per_page = 25
     list_select_related = ('director', 'manager')
-    
-admin.site.register(Module)    
+    inlines = (TaskInline, ModuleInline) 
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project')
+    search_fields = ('name',)
+    list_filter = ('project',)
